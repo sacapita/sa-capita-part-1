f2dbb2f61798176e3b1c3356f0da71eb31c55632
cootje16 <rico557@hotmail.com>
2016-02-10 16:05:08 +0100

    added decomposition effect on contract management

--- gui2/js/custom.js
+++ gui2/js/custom.js
@@ -1,31 +1,25 @@
 $(function(){
-
 	
     initSelect2();
 	scrollToSection();
-	
-	
-  
-      var $section = $('body');
-      var $panzoom = $section.find('.container-fluid').panzoom();
-      $panzoom.parent().on('mousewheel.focal', function( e ) {
-        e.preventDefault();
-        var delta = e.delta || e.originalEvent.wheelDelta;
-        var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-        $panzoom.panzoom('zoom', zoomOut, {
-          increment: 0.1,
-          animate: false,
-          focal: e
-        });
-      });
-      
-      $section.find('.container-fluid').panzoom("pan", -200, -500, "zoom", 0.8);
-        
-      $("a.reset").on('click', function (){
-      	$panzoom.panzoom("resetPan");
-      	$panzoom.panzoom("resetZoom");
-      });
-	
+	panZoom();
+	contractManagement();
+
+	var canvas = document.getElementById("myCanvas");
+			var canvasContext = canvas.getContext('2d');
+
+			canvasContext.beginPath();
+			canvasContext.strokeStyle = '#000';
+			canvasContext.moveTo(-90, -100);
+			canvasContext.lineTo(80, 120);
+			canvasContext.stroke();
+
+			canvasContext.beginPath();
+			canvasContext.moveTo(200, 0);
+			canvasContext.strokeStyle = '#000';
+			canvasContext.lineTo(410, 120);
+			canvasContext.stroke();
+
 });
 
 function highlightText(string){
@@ -84,4 +78,50 @@ function scrollToSection(){
 			break;
 	}
 	
+}
+
+function panZoom(){
+
+	var $section = $('body');
+      var $panzoom = $section.find('.container-fluid').panzoom();
+      $panzoom.parent().on('mousewheel.focal', function( e ) {
+        e.preventDefault();
+        var delta = e.delta || e.originalEvent.wheelDelta;
+        var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+        $panzoom.panzoom('zoom', zoomOut, {
+          increment: 0.1,
+          animate: false,
+          focal: e
+        });
+      });
+      
+      $section.find('.container-fluid').panzoom("pan", 0, 0);
+      $section.find('.container-fluid').panzoom("zoom", 1.0);
+        
+      $("a.reset").on('click', function (){
+      	$panzoom.panzoom("resetPan");
+      	$panzoom.panzoom("resetZoom");
+      });
+}
+
+function contractManagement() {
+	
+		$( "#FAMmodule3" ).click(function() {
+			$("#FAMproductScope").animate({ 'zoom': 0.7 }, 400);
+			$("#myCanvas").show('fast').promise().done(function () {
+				$( "#FAMmodule1Submodule" ).show('fast');
+				$( "#FAMmodule1Submodule" ).animate({
+					zoom: 2.0,
+					"top": "300px",
+				}, 400 );
+
+			});
+
+		});
+
+		$( "#FAMmodule1Submodule .close" ).click(function() {
+			$( "#FAMmodule1Submodule" ).hide('fast');
+			$("#myCanvas").hide('fast');
+			$("#FAMproductScope").animate({ 'zoom': 1.0 }, 400);
+		});
 }
