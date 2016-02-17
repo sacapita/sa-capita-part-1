12e8597a3a10a8eb5ff78c78055a86db77cded50
unknown <bartsmolders111@gmail.com>
2016-02-17 11:20:45 +0100

    js functions to js object, fixed highlight

--- gui2/js/custom.js
+++ gui2/js/custom.js
@@ -1,171 +1,161 @@
-$(function(){
-	$.fn.hasParent=function(e){
-	   return $(this).parents(e).length > 0;
-	}
+var CA = {
+	panzoom: null,
+	containerX: 0,
+	containerY: 0,
+	stopWords: ['As','a','I','want','the','an','to'],
+	construct: function(){
+		CA.panZoom();
+		CA.initSelect2();
+		CA.scrollToSection();
+		CA.initCanvas();
+		CA.contractManagement();
+		CA.initResetPanZoomHandler();
+		CA.initTextboxFocusHandler();
+	},
+	panZoom: function(x,y){
+		x = typeof x !== 'undefined' ? x : this.containerX;
+		y = typeof y !== 'undefined' ? y : this.containerY;
+
+		var $section = $('body');
+		$panzoom = $section.find('.container-fluid').panzoom();
 		
-	//Global variables
-	$panzoom = null;
-	containerX = 0;
-	containerY = 0;
-	
-	//Functions
-    initSelect2();
-	scrollToSection();
-	panZoom();
-	contractManagement();
-	
-	//Handlers
-	initResetPanZoomHandler();
-	
-	$('input').on('click', function(e){
-		$panzoom.panzoom("destroy");
-		$(this).focus();
-	}).on('blur', function(e){
-		var position = $('.container-fluid').position();
-		panZoom(position.left, position.top);		
-	});
-	
-	//Canvas
-	var canvas = document.getElementById("myCanvas");
-	var canvasContext = canvas.getContext('2d');
+		$panzoom.parent().on('mousewheel.focal', function( e ) {
+			e.preventDefault();
+			var delta = e.delta || e.originalEvent.wheelDelta;
+			var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+			$panzoom.panzoom('zoom', zoomOut, {
+				increment: 0.1,
+				animate: false,
+				focal: e
+			});
+		});
 
-	canvasContext.beginPath();
-	canvasContext.strokeStyle = '#000';
-	canvasContext.moveTo(-90, -100);
-	canvasContext.lineTo(80, 120);
-	canvasContext.stroke();
-
-	canvasContext.beginPath();
-	canvasContext.moveTo(200, 0);
-	canvasContext.strokeStyle = '#000';
-	canvasContext.lineTo(410, 120);
-	canvasContext.stroke();
-
-});
-
-function initResetPanZoomHandler(){
-	$(document).bind('keypress', function(event) {	
-		if( event.which === 82 && event.shiftKey && $(document.activeElement).prop("tagName") == "BODY") {			
-			resetPanZoom();
-		}
-	});
-}
-
-function highlightText(string){
-	var tokens = string.split(" ");
-	
-	for(i=0;i<tokens.length;i++){
-		$("div:contains('"+tokens[i]+"')").each(function()
-		{
-			console.log($(this));
-			var $el = $(this);	
-			
-			if(!$el.hasParent('.select2-drop') && !$el.hasClass('.select2-drop') && !$el.hasParent('.select2-container') && !$el.hasClass('.select2-container')){		
-				if(tokens[i] != "a" && tokens[i] != "an" && tokens[i] != "to" && tokens[i] != "I" && tokens[i] != "the" && tokens[i] != "As" && tokens[i] != "event"){
-					console.log($el);
+		$panzoom.panzoom("pan", x, y);
+		$panzoom.panzoom("zoom", 1.0);
+		this.panzoom = $panzoom;
+	},
+	resetPanZoom: function(){	
+		CA.panzoom.panzoom("pan", this.containerX, this.containerY);  
+	},
+	initResetPanZoomHandler: function(){
+		$(document).bind('keypress', function(event) {	
+			if( event.which === 82 && event.shiftKey && $(document.activeElement).prop("tagName") == "BODY") {			
+				CA.resetPanZoom();
+			}
+		});
+	},
+	highlightText: function(string){
+		var tokens = string.split(" ");
+		
+		for(i=0;i<tokens.length;i++){
+			//if(tokens[i] != "a" && tokens[i] != "an" && tokens[i] != "to" && tokens[i] != "I" && tokens[i] != "the" && tokens[i] != "As"){
+			if(this.stopWords.indexOf(tokens[i]) == -1){
+				//Specify the elements in which it is allowed to replace the html/text.
+				$('#functional, .selectedcontainer, #technical, #implementation').each(function()
+				{
+					var $el = $(this);	
 					var regex = new RegExp(tokens[i],"g");		
 					$el.html( $el.html().replace(regex, "<span style=\"text-decoration: underline\">"+tokens[i]+"</span>") );
-				}
-		   	}
+					var regex = new RegExp(tokens[i].capitalizeFirstLetter(),"g");	
+					$el.html( $el.html().replace(regex, "<span style=\"text-decoration: underline\">"+tokens[i].capitalizeFirstLetter() +"</span>") );
+				});
+			}
+		}
+	},
+	initSelect2: function(){
+		var data = [
+			{ id: 0, text: 'As a manager, I want to create an event'}, 
+			{ id: 1, text: 'As a manager, I want to publish the line-up' }, 
+			{ id: 2, text: 'As a manager, I want to change the event date' }, 
+			{ id: 3, text: 'As a manager, I want to fix Legal stuff' },
+			{ id: 4, text: 'As a manager, I want to indicate a price' }
+			];
+		
+		$('#searchreqs').select2({
+			placeholder: "Select a user story",
+			allowClear: true,
+			data: { results: data},
+			multiple: true
+		}).on("select2-selecting", function(e) {
+			$("#selectedreqs").append("<li class="+e.choice.id+">"+e.choice.text+"</li>");
+			CA.highlightText(e.choice.text);
+		}).on("select2-removed", function(e) {
+			$("#selectedreqs li."+e.choice.id).remove();
 		});
-	}
-	/*
-	$('.select2-drop ul li div').each(function(){
-		var text = $(this).text();
-		$(this).html("<span class=\"select2-match\"></span>" + text);
-	});*/
-}
+	},
 
-function initSelect2(){
-	var data = [
-		//{ id: 0, text: 'As a manager, I want to create an event'}, 
-		//{ id: 1, text: 'As a manager, I want to publish the line-up' }, 
-		//{ id: 2, text: 'As a manager, I want to change the event date' }, 
-		{ id: 3, text: 'As a manager, I want to fix Legal stuff' } 
-		//{ id: 4, text: 'As a manager, I want to indicate a price' }
-		];
-	
-	$('#searchreqs').select2({
-		placeholder: "Select a user story",
-    	allowClear: true,
-    	data: { results: data},
-    	multiple: true
-    }).on("select2-selecting", function(e) {
-    	$("#selectedreqs").append("<li class="+e.choice.id+">"+e.choice.text+"</li>");
-    	highlightText(e.choice.text);
-    }).on("select2-removed", function(e) {
-    	$("#selectedreqs li."+e.choice.id).remove();
-   	});
-}
+	scrollToSection: function(){
+		var section = window.location.hash;
+		switch(section){
+			case "#developer":
+				$("html").scrollTo($("#implementation"), {duration: 500});
+				console.log("dev");
+				break;
+			case "#architect":
+				$(".content").scrollTo("#functional", 300);
+				console.log("archi");
+				break;
+			case "#manager":
+				$(".content").scrollTo("#requirements", 300);
+				console.log("manager");
+				break;
+			default:
+				break;
+		}
+		
+	},
+	contractManagement: function() {
+		
+			$( "#FAMmodule3" ).click(function() {
+				$("#FAMproductScope").animate({ 'zoom': 0.7 }, 400);
+				$("#myCanvas").show('fast').promise().done(function () {
+					$( "#FAMmodule1Submodule" ).show('fast');
+					$( "#FAMmodule1Submodule" ).animate({
+						zoom: 2.0,
+						"top": "300px",
+					}, 400 );
 
-function scrollToSection(){
-	var section = window.location.hash;
-	switch(section){
-		case "#developer":
-			$("html").scrollTo($("#implementation"), {duration: 500});
-			console.log("dev");
-			break;
-		case "#architect":
-			$(".content").scrollTo("#functional", 300);
-			console.log("archi");
-			break;
-		case "#manager":
-			$(".content").scrollTo("#requirements", 300);
-			console.log("manager");
-			break;
-		default:
-			break;
-	}
-	
-}
-
-function panZoom(x,y){
-	x = typeof x !== 'undefined' ? x : containerX;
-	y = typeof y !== 'undefined' ? y : containerY;
-
-	var $section = $('body');
-    $panzoom = $section.find('.container-fluid').panzoom();
-	
-	$elem = $("#requirements");
-	$elem.panzoom("disable");
-	
-	$panzoom.parent().on('mousewheel.focal', function( e ) {
-		e.preventDefault();
-		var delta = e.delta || e.originalEvent.wheelDelta;
-		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
-		$panzoom.panzoom('zoom', zoomOut, {
-			increment: 0.1,
-			animate: false,
-			focal: e
-		});
-	});
-
-	$panzoom.panzoom("pan", x, y);
-	$panzoom.panzoom("zoom", 1.0);	
-}
-
-function resetPanZoom(){	
-	$panzoom.panzoom("pan", containerX, containerY);  
-}
-
-function contractManagement() {
-	
-		$( "#FAMmodule3" ).click(function() {
-			$("#FAMproductScope").animate({ 'zoom': 0.7 }, 400);
-			$("#myCanvas").show('fast').promise().done(function () {
-				$( "#FAMmodule1Submodule" ).show('fast');
-				$( "#FAMmodule1Submodule" ).animate({
-					zoom: 2.0,
-					"top": "300px",
-				}, 400 );
+				});
 
 			});
 
-		});
+			$( "#FAMmodule1Submodule .close" ).click(function() {
+				$( "#FAMmodule1Submodule" ).hide('fast');
+				$("#myCanvas").hide('fast');
+				$("#FAMproductScope").animate({ 'zoom': 1.0 }, 400);
+			});
+	},
+	initCanvas: function(){
+		var canvas = document.getElementById("myCanvas");
+		var canvasContext = canvas.getContext('2d');
 
-		$( "#FAMmodule1Submodule .close" ).click(function() {
-			$( "#FAMmodule1Submodule" ).hide('fast');
-			$("#myCanvas").hide('fast');
-			$("#FAMproductScope").animate({ 'zoom': 1.0 }, 400);
+		canvasContext.beginPath();
+		canvasContext.strokeStyle = '#000';
+		canvasContext.moveTo(-90, -100);
+		canvasContext.lineTo(80, 120);
+		canvasContext.stroke();
+
+		canvasContext.beginPath();
+		canvasContext.moveTo(200, 0);
+		canvasContext.strokeStyle = '#000';
+		canvasContext.lineTo(410, 120);
+		canvasContext.stroke();
+	},
+	initTextboxFocusHandler: function(){
+		$('input,textbox,select').on('click', function(e){
+			CA.panzoom.panzoom("destroy");
+			$(this).focus();
+		}).on('blur', function(e){
+			var position = $('.container-fluid').position();
+			CA.panZoom(position.left, position.top);		
 		});
-}
+	}
+};
+
+$(function(){
+	CA.construct();
+	
+	String.prototype.capitalizeFirstLetter = function() {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}		
+});
